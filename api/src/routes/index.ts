cce1956c2012ac0e1ac9dc9afccabaded5c9bbcc
rickyluo <hualazimo@live.cn>
2024-08-08 12:03:07 +0800

    init

---
+++ api/src/routes/index.ts
@@ -0,0 +1,14 @@
+import middleware from '@blocklet/sdk/lib/middlewares';
+import { Router } from 'express';
+
+const router = Router();
+
+router.use('/user', middleware.user(), (req, res) => res.json(req.user || {}));
+
+router.use('/data', (_, res) =>
+  res.json({
+    message: 'Hello Blocklet!',
+  }),
+);
+
+export default router;
